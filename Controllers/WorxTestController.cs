bb2e6831342b4b2bdbd0369c997b5cae958d6893
Seems <Seemara_Kim@worxmachinery.com>
2024-10-23 14:25:26 -0400

    Add project files.

---
+++ Controllers/WorxTestController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+using Newtonsoft.Json;
+
+namespace WorxTESTWebApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WorxTestController : Controller
+    {
+        [HttpGet]
+        public string GetWorkOrderJSON(string WorkOrderNum)
+        {
+            string RawJSONResult = "Empty";
+            //PLACEHOLDER FOR RETRIEVING RAW JSON STRING
+            //Retrieving JSON file with queried work order number=============================================
+            foreach (var item in Directory.GetFiles("C:\\Users\\Seems\\Documents\\Custom Projects\\Arthrex"))
+            {
+                if (item.Contains(WorkOrderNum))
+                {
+                    RawJSONResult = System.IO.File.ReadAllText(item);
+                }
+            }
+            //================================================================================================
+            return RawJSONResult;          
+        }     
+    }
+}
